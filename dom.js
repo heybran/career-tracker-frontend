fae609c39c284145d13de20dbb7a69db8e38f7ff
Brandon Zhang <gwonzhang@gmail.com>
2024-04-06 10:59:52 +0800

    Add rich text editor

--- dom.js
+++ dom.js
@@ -44,24 +44,54 @@ export function renderTable(jobs) {
 
 export function renderFormElements(job, layout) {
   layout.innerHTML = `
-    <cc-text-field label="Website" name="website" required value="${decodeURIComponent(job.website)}"></cc-text-field>
-    <cc-text-field label="Position" name="position" required value="${job.position}"></cc-text-field>
-    <cc-text-field label="Source" name="source" value="${job.source}"></cc-text-field>
-    <cc-date-picker label="Date Applied" required name="date_applied" value="${job.date_applied}"></cc-date-picker>
+    <cc-text-field label="Website" name="website" required value="${decodeURIComponent(
+      job.website
+    )}"></cc-text-field>
+    <cc-text-field label="Position" name="position" required value="${
+      job.position
+    }"></cc-text-field>
+    <cc-text-field label="Source" name="source" value="${
+      job.source
+    }"></cc-text-field>
+    <cc-date-picker label="Date Applied" required name="date_applied" value="${
+      job.date_applied
+    }"></cc-date-picker>
     <cc-select name="status" label="Status" colspan="2">
-      <cc-option value="pending" aria-selected="${job.status === 'pending' ? 'true' : 'false'}">Pending</cc-option>
-      <cc-option value="rejected" aria-selected="${job.status === 'rejected' ? 'true' : 'false'}">Rejected</cc-option>
-      <cc-option value="not-applied" aria-selected="${job.status === 'not-applied' ? 'true' : 'false'}">Not Applied</cc-option>
+      <cc-option value="pending" aria-selected="${
+        job.status === "pending" ? "true" : "false"
+      }">Pending</cc-option>
+      <cc-option value="rejected" aria-selected="${
+        job.status === "rejected" ? "true" : "false"
+      }">Rejected</cc-option>
+      <cc-option value="not-applied" aria-selected="${
+        job.status === "not-applied" ? "true" : "false"
+      }">Not Applied</cc-option>
     </cc-select>
-    <cc-textarea label="Notes" name="notes" colspan="2" helper-text="Maximum characters allowed: 3000"></cc-textarea>
+    <div colspan="2">
+      <label for="notes" style="font-weight: 600;">Notes</label>
+      <input id="notes" type="hidden" name="notes" value="${job.notes}">
+      <trix-editor input="notes" class="trix-content"></trix-editor>
+    </div>
     <cc-radio-group label="Apply channel" name="apply_channel" colspan="2">
-      <cc-radio label="Email" value="email" ${job.apply_channel === 'email' ? 'checked' : ''}></cc-radio>
-      <cc-radio label="Job application form" value="job-application-form" ${job.apply_channel === 'job-application-form' ? 'checked' : ''}></cc-radio>
-      <cc-radio label="LinkedIn" value="linkedin" ${job.apply_channel === 'linkedin' ? 'checked' : ''}></cc-radio>
+      <cc-radio label="Email" value="email" ${
+        job.apply_channel === "email" ? "checked" : ""
+      }></cc-radio>
+      <cc-radio label="Job application form" value="job-application-form" ${
+        job.apply_channel === "job-application-form" ? "checked" : ""
+      }></cc-radio>
+      <cc-radio label="LinkedIn" value="linkedin" ${
+        job.apply_channel === "linkedin" ? "checked" : ""
+      }></cc-radio>
     </cc-radio-group>
-    <cc-checkbox name="volunteer_position" label="Volunteer position" ${Number(job.volunteer_position) !== 0 ? 'checked' : ''}></cc-checkbox>
-    <cc-checkbox name="freelance" label="Freelance" ${Number(job.freelance) !== 0 ? 'checked' : ''}></cc-checkbox>
-    <cc-checkbox name="four_day_week" label="4 day week" ${Number(job.four_day_week) !== 0 ? 'checked' : ''}></cc-checkbox>
+    <cc-checkbox name="volunteer_position" label="Volunteer position" ${
+      Number(job.volunteer_position) !== 0 ? "checked" : ""
+    }></cc-checkbox>
+    <cc-checkbox name="freelance" label="Freelance" ${
+      Number(job.freelance) !== 0 ? "checked" : ""
+    }></cc-checkbox>
+    <cc-checkbox name="four_day_week" label="4 day week" ${
+      Number(job.four_day_week) !== 0 ? "checked" : ""
+    }></cc-checkbox>
   `;
 
   layout.querySelector('cc-textarea').value = job.notes;
