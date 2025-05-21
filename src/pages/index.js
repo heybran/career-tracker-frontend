5dff141a10697a991169694d0f8e752ebf5e14ca
Brandon Zhang <gwonzhang@gmail.com>
2025-05-21 10:26:30 +0800

    add project not working notice

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import { UtilsMixin } from "utils/index.js";
 
 export default class HomePage extends UtilsMixin(HTMLElement) {
-  static tagName = "home-page";
-  connectedCallback() {
-    this.innerHTML = `
+	static tagName = "home-page";
+	connectedCallback() {
+		this.innerHTML = `
         <style>
             home-page {
                 height: 80vh;
@@ -22,15 +22,16 @@ export default class HomePage extends UtilsMixin(HTMLElement) {
                 <img src="/public/career-tracker.svg" alt="Career Tracker Logo" class="logo">
                 <cc-visually-hidden>Career Tracker</cc-visually-hidden>
             </h1>
-        <p>A tiny web app that helps you track the jobs you applied, built with vanilla JavaScript & Web Components on frontend and WordPress & PHP as backend.</p>
-        <cc-horizontal-layout style="justify-content: center;">
-            <cc-button href="/signup" theme="primary">Sign up</cc-button>
-            <cc-button href="/signin" theme="neutral">Sign in</cc-button>
-            <cc-button href="/signin?email=demo@careertracker.work&password=demo" theme="success">Demo</cc-button>
-        </cc-horizontal-layout>
+        	<p>A tiny web app that helps you track the jobs you applied, built with vanilla JavaScript & Web Components on frontend and WordPress & PHP as backend.</p>
+			<cc-horizontal-layout style="justify-content: center;">
+				<cc-button href="/signup" theme="primary">Sign up</cc-button>
+				<cc-button href="/signin" theme="neutral">Sign in</cc-button>
+				<cc-button href="/signin?email=demo@careertracker.work&password=demo" theme="success">Demo</cc-button>
+			</cc-horizontal-layout>
+			<p style="color: red;">This project is no longer working due to previous domain "career-tracker.work" has expired and it now has CORS policy issue with the API.</p>
         </div>
     `;
-  }
+	}
 }
 
 customElements.define(HomePage.tagName, HomePage);
